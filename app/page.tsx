a73e21f5ad439763b157e6b18daf4e177e667006
Viktor Kindrat <kindratvictor07@gmail.com>
2024-07-06 18:36:43 +0300

    dyncamic imports and code optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 
 const DynamicAboutUs = dynamic(() => import("@/app/ui/home/aboutUs/aboutUs"), {
     loading: () => <div style={{height: "100vh"}}></div>,
+    ssr: false
 })
 
 export default function Home() {
