401e9de6b8c7916980b5c36a806861fa66095343
Viktor Kindrat <kindratvictor07@gmail.com>
2024-07-07 21:36:39 +0300

    update. off animation on mobiles

--- app/ui/home/aboutUs/aboutUs.tsx
+++ app/ui/home/aboutUs/aboutUs.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import backgroundPattern from "@/app/ui/home/aboutUs/SVG/backgroundPattern.svg";
-import {useEffect, useMemo, useState, useCallback} from "react";
+import React, {useEffect, useMemo, useState, useCallback} from "react";
 import Image from "next/image";
 
 import styles from "@/app/ui/home/aboutUs/aboutUs.module.css";
 import headerStyles from "@/app/ui/header/header.module.css";
-import Content from "./content";
 import {contentData as content} from "./content";
 
 import {gsap} from "gsap";
@@ -23,9 +22,22 @@ gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
 export default function AboutUs() {
     const [headerHeight, setHeaderHeight] = useState(50);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const userAgent = navigator.userAgent || navigator.vendor;
+
+        if (/android/i.test(userAgent)) {
+            setIsMobile(true);
+        } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    }, []);
 
     const stages = useMemo(
-        () => [
+        () => !isMobile ? [
             [
                 [`50px`, `calc(50px + ${headerHeight}px)`, `translate(0, 0)`],
                 [`calc(100% - 50px)`, `calc(100% - 50px)`, `translate(-100%, -100%)`],
@@ -42,8 +54,8 @@ export default function AboutUs() {
                 [`50px`, `100%`, `translate(0, -100%)`],
                 [`calc(100% - 50px)`, `calc(50px + ${headerHeight}px)`, `translate(-100%, 0)`],
             ],
-        ],
-        [headerHeight]
+        ] : [],
+        [headerHeight, isMobile]
     );
 
     const handleResize = useCallback(() => {
@@ -61,6 +73,8 @@ export default function AboutUs() {
     }, [handleResize]);
 
     useEffect(() => {
+        let handleMouseMove: (event: MouseEvent) => void;
+
         gsap.defaults({ease: "none"});
 
         gsap.to(`.${styles.background}`, {
@@ -75,30 +89,30 @@ export default function AboutUs() {
             },
         });
 
-        content.forEach((_, i) => {
-            const animElement = document.querySelector(`.${styles.anim}${i + 1}`);
+        if (!isMobile) {
+            content.forEach((_, i) => {
+                const animElement = document.querySelector(`.${styles.anim}${i + 1}`);
 
-            if (animElement) {
-                const height = animElement.clientHeight;
+                if (animElement) {
+                    const height = animElement.clientHeight;
 
-                const tl = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: `.${styles.container}${i + 1}`,
-                        pin: animElement,
-                        start: "top center",
-                        end: "+=100px",
-                        toggleActions: "restart none reverse restart",
-                        scrub: 2,
-                        pinSpacing: false,
-                    },
-                });
+                    const tl = gsap.timeline({
+                        scrollTrigger: {
+                            trigger: `.${styles.container}${i + 1}`,
+                            pin: `.${styles.anim}${i + 1}`,
+                            start: "top center",
+                            end: "+=100px",
+                            toggleActions: "restart none reverse restart",
+                            scrub: 2,
+                            pinSpacing: false,
+                        },
+                    });
 
-                const prevStage = stages[i - 1];
-                const currStage = stages[i];
+                    const prevStage = stages[i - 1];
+                    const currStage = stages[i];
 
-                if (prevStage && currStage) {
-                    tl.fromTo(animElement, {opacity: 0}, {opacity: 1, duration: 0.3})
-                        .fromTo(
+                    if (prevStage && currStage) {
+                        tl.fromTo(
                             `.${styles.backgroundItem1}`,
                             {
                                 left: prevStage[0][0],
@@ -112,92 +126,97 @@ export default function AboutUs() {
                                 duration: 5,
                             }
                         )
-                        .fromTo(
-                            `.${styles.backgroundItem2}`,
-                            {
-                                left: prevStage[1][0],
-                                top: prevStage[1][1],
-                                transform: prevStage[1][2],
+                            .fromTo(
+                                `.${styles.backgroundItem2}`,
+                                {
+                                    left: prevStage[1][0],
+                                    top: prevStage[1][1],
+                                    transform: prevStage[1][2],
+                                },
+                                {
+                                    left: currStage[1][0],
+                                    top: currStage[1][1],
+                                    transform: currStage[1][2],
+                                    duration: 5,
+                                },
+                                0
+                            );
+                    }
+
+                    gsap.fromTo(
+                        `.${styles.anim}${i + 1} .${styles.animScrollHint}`,
+                        {opacity: 0},
+                        {
+                            opacity: 1,
+                            delay: 15,
+                            duration: 1,
+                            scrollTrigger: {
+                                trigger: `.${styles.container}${i + 1}`,
+                                start: "top center",
+                                toggleActions: "restart none reverse restart",
                             },
-                            {
-                                left: currStage[1][0],
-                                top: currStage[1][1],
-                                transform: currStage[1][2],
-                                duration: 5,
+                        }
+                    );
+
+                    gsap.fromTo(
+                        animElement,
+                        {opacity: 1},
+                        {
+                            opacity: 0,
+                            scrollTrigger: {
+                                trigger: `.${styles.container}${i + 1}`,
+                                pin: false,
+                                start: `85%-=${height}px center`,
+                                end: "85% center",
+                                toggleActions: "restart complete reverse reverse",
+                                scrub: 0.5,
+                                pinSpacing: false,
+                                anticipatePin: 1,
                             },
-                            0
-                        );
+                        }
+                    );
                 }
+            });
 
-                gsap.fromTo(
-                    `.${styles.anim}${i + 1} .${styles.animScrollHint}`,
-                    {opacity: 0},
-                    {
-                        opacity: 1,
-                        delay: 15,
-                        duration: 1,
-                        scrollTrigger: {
-                            trigger: `.${styles.container}${i + 1}`,
-                            start: "top center",
-                            toggleActions: "restart none reverse restart",
-                        },
-                    }
-                );
+            const blocks = document.querySelectorAll(`.${styles.backgroundStatic}`);
+            let animationFrameId: number;
 
-                gsap.fromTo(
-                    animElement,
-                    {opacity: 1},
-                    {
-                        opacity: 0,
-                        scrollTrigger: {
-                            trigger: `.${styles.container}${i + 1}`,
-                            pin: false,
-                            start: `85%-=${height}px center`,
-                            end: "85% center",
-                            toggleActions: "restart complete reverse reverse",
-                            scrub: 0.5,
-                            pinSpacing: false,
-                            anticipatePin: 1,
-                        },
-                    }
-                );
-            }
-        });
+            handleMouseMove = (event: MouseEvent) => {
+                cancelAnimationFrame(animationFrameId);
+                animationFrameId = requestAnimationFrame(() => {
+                    const mouseX = event.clientX;
+                    const mouseY = event.clientY;
 
-        const blocks = document.querySelectorAll(`.${styles.backgroundStatic}`);
-        let animationFrameId: number;
+                    blocks.forEach((block, idx) => {
+                        const blockRect = block.getBoundingClientRect();
+                        const blockCenterX = blockRect.left + blockRect.width / 2;
+                        const blockCenterY = blockRect.top + blockRect.height / 2;
 
-        const handleMouseMove = (event: MouseEvent) => {
-            cancelAnimationFrame(animationFrameId);
-            animationFrameId = requestAnimationFrame(() => {
-                const mouseX = event.clientX;
-                const mouseY = event.clientY;
+                        const distanceX = mouseX - blockCenterX;
+                        const distanceY = mouseY - blockCenterY;
 
-                blocks.forEach((block, idx) => {
-                    const blockRect = block.getBoundingClientRect();
-                    const blockCenterX = blockRect.left + blockRect.width / 2;
-                    const blockCenterY = blockRect.top + blockRect.height / 2;
-
-                    const distanceX = mouseX - blockCenterX;
-                    const distanceY = mouseY - blockCenterY;
-
-                    gsap.to(block, {
-                        x: (-1) ** idx * (distanceX / 10),
-                        y: (-1) ** idx * (distanceY / 10),
-                        duration: 0.2,
+                        gsap.to(block, {
+                            x: (-1) ** idx * (distanceX / 10),
+                            y: (-1) ** idx * (distanceY / 10),
+                            duration: 0.2,
+                        });
                     });
                 });
-            });
-        };
+            };
 
-        document.addEventListener("mousemove", handleMouseMove);
+            document.addEventListener("mousemove", handleMouseMove);
+        } else {
+            gsap.to(`.${styles.anim}`, {
+                opacity: 1
+            })
+        }
 
         return () => {
             ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
             gsap.globalTimeline.clear();
             document.removeEventListener("mousemove", handleMouseMove);
         };
-    }, [headerHeight, stages]);
+    }, [headerHeight, stages, isMobile]);
 
     return (
         <div id="Home-about-us" className={styles.AboutUs}>
