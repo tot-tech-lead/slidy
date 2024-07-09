bce21f2226a5b8806b6da7ff5d19f5b21e66a03b
Viktor Kindrat <kindratvictor07@gmail.com>
2024-07-09 18:58:55 +0300

    WaitList styles

--- app/ui/home/waitList/wait-list.tsx
+++ app/ui/home/waitList/wait-list.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import styles from "./wait-list.module.css"
+import prestyle from "@/app/lib/ui-components.module.css"
+import {nunito} from "@/app/ui/fonts";
+
 import steps from "./SVG/steps.svg";
 import plane from "./SVG/plane.svg";
 import Image from "next/image";
 
 import {appendUser, State} from "@/app/lib/action/wait-list";
 import {useActionState, useEffect} from "react"
+import clsx from "clsx";
 
 
 export default function WaitListForm() {
@@ -20,36 +24,38 @@ export default function WaitListForm() {
     }, [state])
 
     return (
-        <div className="WaitListForm">
-            <div className="WaitListForm__background">
-                <Image src={steps} alt="сліди" className="WaitListForm__background-1"/>
-                <Image src={steps} alt="slidy" className="WaitListForm__background-2"/>
+        <div className={styles.WaitListForm}>
+            <div className={styles.background}>
+                <Image src={steps} alt="сліди" className={styles.background1}/>
+                <Image src={steps} alt="slidy" className={styles.background2}/>
             </div>
-            <div className="WaitListFrom__wrapper">
-                <h2 className="WaitListForm__h2">
+            <div className={styles.wrapper}>
+                <h2 className={`${styles.h2} ${prestyle.textH2} ${nunito.className}`}>
                     ⚡️ Вже кортить все спробувати?
                 </h2>
 
-                <p className="WaitListForm__paragraph">
+                <p className={`${styles.paragraph} ${prestyle.textBig}`}>
                     Поки що, наш сайт перебуває на стадії розробки. Ми активно працюємо заради того, щоб ви могли
                     покращити свої подорожі якомога швидше. Якщо ви хочете дізнатись про завершення робіт першими,
                     зашилште свою електронну адресу у полі нижче.
                     <br/>Після того як все буде готово, ми зв&apos;яжемося з вами за цією адресою.
                 </p>
 
-                <form className="WaitListForm__form"
+                <form className={styles.form}
                       action={formAction}
                       aria-describedby="email-error"
                 >
                     <input
-                        className="WaitListForm__input" type="email" placeholder="Ваша електронна адреса" name="email" required={true}
+                        className={`${styles.input} ${prestyle.textPlain}`} type="email" placeholder="Ваша електронна адреса" name="email" required={true}
                     />
-                    <button aria-disabled={pending} disabled={pending} className="WaitListForm__button" type="submit">
-                        <span className="Button__text">Сповістіть мене!</span>
-                        <Image className="WaitList__btn-img" src={plane} alt="plane" height="25" width="25"/>
+                    <button aria-disabled={pending} disabled={pending} className={`${styles.button} ${prestyle.buttonFilled}`} type="submit">
+                        <span className={styles.buttonText}>Сповістіть мене!</span>
+                        <Image className={styles.btnImg} src={plane} alt="plane" height="25" width="25"/>
                     </button>
                 </form>
-                <div id="email-error" aria-live="polite" aria-atomic="true">
+                <div className={clsx(`${styles.alarm} ${prestyle.textPlain}`, {
+                    [styles.alarmGreen] : state.status === 409
+                })} id="email-error" aria-live="polite" aria-atomic="true">
                     {(state.status !== 200 && state.message) ? state.message : ""}
                 </div>
             </div>
