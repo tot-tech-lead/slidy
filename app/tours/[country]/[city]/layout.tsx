0784f3afda8b10073fbdfa4213d0f0402340cb9a
Viktor Kindrat <kindratvictor07@gmail.com>
2024-07-13 20:01:56 +0300

    fixes

---
+++ app/tours/[country]/[city]/layout.tsx
@@ -0,0 +1,51 @@
+import type {Metadata} from "next";
+
+import styles from "./tours.module.css"
+import prestyle from "@/app/lib/ui-components.module.css"
+
+import Filter from "@/app/ui/filter/filter"
+import clsx from "clsx";
+import {nunito} from "@/app/ui/fonts";
+import React from "react";
+
+export default function Layout({children}: {
+    children: React.ReactNode;
+}) {
+
+    return (
+        <>
+            <div className={styles.Tours}>
+                <div className={styles.headlineGroup}>
+                    <Filter
+                        filters={[
+                            {
+                                type: "text",
+                                name: "category",
+                                label: "Категорія",
+                            }, {
+                                type: "text",
+                                name: "duration",
+                                label: "Тривалість (год)",
+                            }, {
+                                type: "text",
+                                name: "countOfPeople",
+                                label: "Кількість людей",
+                            }, {
+                                type: "text",
+                                name: "price",
+                                label: "Вартість",
+                            },
+                        ]}
+                    />
+                    <h2 className={clsx(styles.headline, prestyle.textH2, nunito.className)}>Результати пошуку</h2>
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const metadata: Metadata = {
+    title: "Slidy - доступні екскурсії",
+    robots: "all"
+};
